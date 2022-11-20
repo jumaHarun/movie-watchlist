f8a00ff54b263393729be8e0afc5ade8710f7558
Juma Harun <mmbh4run@gmail.com>
2022-11-20 07:19:09 +0300

    refactor

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
-const movieWrapper = document.getElementById('movieWrapper')
-const watchlistEl = document.getElementById('watchlistWrapper')
 const searchInput = document.getElementById('search')
-
-let watchlist = []
+const apiKey = '8b0ff547'
 
 document.addEventListener('click', (e) => {
-  if (e.target.dataset.search) {
-    const query = searchInput.value
-    let html = ''
-    fetch(`https://www.omdbapi.com/?apikey=8b0ff547&s=${query}`)
-      .then(res => res.json())
-      .then(data => {
-        data.Search.map(movie => {
-          fetch(`https://www.omdbapi.com/?apikey=8b0ff547&t=${movie.Title}`)
-            .then(res => res.json())
-            .then(data => {
-              if (!data.Error) {
-                html += getMovieHtml(data)
-                renderHome(html)
-              }
-            })
-        })
-      })
-      .catch(err => console.log(err))
+  if (e.target.dataset.button) {
+    if (e.target.dataset.button === 'search') {
+      handleSearchBtnClick()
+    }
+
+    if (e.target.dataset.button === 'add') {
+      handleAddBtnClick()
+    }
+
+    if (e.target.dataset.button === 'remove') {
+      handleRemoveBtnClick()
+    }
   }
-
-  if (e.target.dataset.add) { }
-
-  if (e.target.dataset.remove) { }
 })
 
+function handleSearchBtnClick() {
+  let html = ''
 
-function renderHome(html) {
-  if (movieWrapper) {
-    movieWrapper.innerHTML = html
-  }
+  fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchInput.value}`)
+    .then(res => res.json())
+    .then(data => {
+      data.Search.map(movie => {
+        fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${movie.Title}`)
+          .then(res => res.json())
+          .then(data => {
+            if (!data.Error) {
+              html += getMoviesHtml(data)
+              render(html)
+            }
+          })
+      })
+    })
+    .catch(err => console.log(`API ERROR: ${err}`))
+}
+
+function handleAddBtnClick() {
+  console.log('Add to watchlist')
+}
+
+function handleRemoveBtnClick() {
+  console.log('Remove from watchlist')
 }
 
 
-function getMovieHtml(data) {
-  const { Title, Poster, imdbRating, Runtime, Genre, Plot } = data
+function getMoviesHtml(movieObj) {
+  const { Title, Year, Poster, imdbRating, Runtime, Genre, Plot, Type } = movieObj
 
   return `
   <div class="data-grid">
@@ -49,11 +57,12 @@ function getMovieHtml(data) {
     
     <div class="data-details">
       <div class="movie-title">
-        <h3 class="title">${Title}</h3>
+        <h3 class="title"></h3>
+        <h3 class="title">${Title}<span class="year"> &#9900 ${Year}</span></h3>
 
         <div class="movie-rating">
           <img src="/images/star.png" alt="a star image to show rating" class="rating-img">
-          <p class="rating">${imdbRating}</p>
+          <p class="rating">${imdbRating} <span class="type"> &#9900 ${Type}</span></p>
         </div>
       </div>
 
@@ -61,7 +70,7 @@ function getMovieHtml(data) {
         <p class="duration">${Runtime}</p>
         <p class="genre">${Genre}</p>
         <div class="add-wrapper">
-          <img src="/images/add-icon.png" alt="add icon" class="add-icon" id="addBtn">
+          <img src="/images/add-icon.png" alt="add icon" class="add-icon" data-button="add">
           <p>Watchlist</p>
         </div>
       </div>
@@ -75,4 +84,8 @@ function getMovieHtml(data) {
   </div>
   <hr>
   `
+}
+
+function render(html) {
+  document.getElementById('movieWrapper').innerHTML = html
 }
